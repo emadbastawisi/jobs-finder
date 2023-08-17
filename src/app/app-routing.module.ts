dba81a96aae5b01176952c8460b278eb78eceb10
emadbastawisi <emadbastawisi@outlook.com>
2023-08-17 09:43:50 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/feature/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'clients',
+    loadChildren: () =>
+      import('./clients/feature/clients-shell/client-shell.module').then(
+        (m) => m.ClientShellModule
+      ),
+  },
+
+  {
+    path: 'jobs',
+    loadChildren: () =>
+      import('./jobs/feature/jobs-shell/jobs-shell.module').then(
+        (m) => m.JobsShellModule
+      ),
+  },
+  {
+    path: 'categories',
+    loadChildren: () =>
+      import('./categories/feature/categories-shell/categories-shell.module').then(
+        (m) => m.CategoriesShellModule
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolBarModule } from 'src/app/shared/ui/tool-bar.module';
+
+
+import { HttpClientModule } from '@angular/common/http';
+import { SearchListComponent } from './search-list/search-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchListComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToolBarModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+
   ],
-  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [provideStore()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
