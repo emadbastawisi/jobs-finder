e7cb03b31c3b7118422b99cbc1436e17904767ec
emadbastawisi <emadbastawisi@outlook.com>
2023-08-28 18:56:15 +0300

    added standalone datepicker component

---
+++ src/app/shared/ui/date-picker/date-picker.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@Component({
+  selector: 'app-date-picker',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+  ],
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.css'],
+})
+export class DatePickerComponent {
+  @Input() label: string = '';
+  maxDate: Date;
+  minDate: Date;
+
+  constructor() {
+    // Set the minimum to January 1st 20 years in the past and December 31st a year in the future.
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 80, 0, 1);
+    this.maxDate = new Date(currentYear - 18, 0, 1);
+  }
+}
