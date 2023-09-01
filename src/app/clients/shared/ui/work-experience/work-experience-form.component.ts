3721a25240f4d9ce8b68e98ce2e626c5f8ddd45e
emadbastawisi <emadbastawisi@outlook.com>
2023-09-02 01:58:40 +0300

    created work experience foem component

---
+++ src/app/clients/shared/ui/work-experience/work-experience-form.component.ts
@@ -0,0 +1,28 @@
+import { I } from '@angular/cdk/keycodes';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import * as list from '../../utils/list';
+
+@Component({
+  selector: 'app-work-experience-form',
+  templateUrl: './work-experience-form.component.html',
+  styleUrls: ['./work-experience-form.component.css'],
+})
+export class WorkExperienceFormComponent {
+  @Input() FormGroup!: FormGroup;
+  experienceTypeList = list.experienceTypeList;
+  jobCategoryList = list.categoriesList;
+  yearsOfExperienceList = list.yearsOfExperienceList;
+
+  getControl(controlName: string): FormControl {
+    return this.FormGroup.get(controlName) as FormControl;
+  }
+  onWorkThereChange(event: any) {
+    console.log(event);
+    if (event.checked) {
+      this.FormGroup.removeControl('end_date');
+    } else {
+      this.FormGroup.addControl('end_date', new FormControl(''));
+    }
+  }
+}
---
+++ src/app/clients/shared/ui/work-experience/work-experience-form.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkExperienceFormComponent } from './work-experience-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChipsAutocompleteComponent } from 'src/app/shared/ui/chipsAutocomplete/chips-autocomplete.component';
+import { DatePickerComponent } from 'src/app/shared/ui/date-picker/date-picker.component';
+import { InputComponent } from 'src/app/shared/ui/input/input.component';
+import { ChipsComponent } from 'src/app/shared/ui/chips/chips.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+@NgModule({
+  declarations: [WorkExperienceFormComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ChipsAutocompleteComponent,
+    DatePickerComponent,
+    InputComponent,
+    ChipsComponent,
+    MatCheckboxModule,
+  ],
+  exports: [WorkExperienceFormComponent],
+})
+export class WorkExperienceFormModule {}
