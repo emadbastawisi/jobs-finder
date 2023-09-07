9bc014953590d0e45868be18a583e69b90fed059
emadbastawisi <emadbastawisi@outlook.com>
2023-09-07 16:21:44 +0300

    added loading skeleton to jobs card

--- src/app/jobs/feature/jobs-list/jobs-list.component.ts
+++ src/app/jobs/feature/jobs-list/jobs-list.component.ts
@@ -23,6 +23,8 @@ export class JobsListComponent implements OnInit {
   onGetJobs() {
     this.jobsService.getClientJobs(this.selectedKeywords);
   }
+
+  defaultJobs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 }
 
 
